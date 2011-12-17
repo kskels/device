80670754d7be56fcb390a83636f5ba88cda6969c
Kaspars <kaspars.skels@gmail.com>
2011-12-17 20:38:58 +0100

    Added boost program options to the main executable

--- src/core/core.cpp
+++ src/core/core.cpp
@@ -11,34 +11,80 @@
 #include <stdio.h>
 #include <sys/shm.h>
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+
+void options(int argc, char* argv[])
+{
+    po::options_description desc("Generic");
+    desc.add_options()
+        ("help,h", "display help message")
+        ("version,v", "display version information")
+    ;
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc;
+        return 1;
+    }
+    if (vm.count("version")) {
+        std::cout << "0.1.1" << std::endl;
+        return 0;
+    }
+
+    po::options_description env("Environment");
+    env.add_options()
+        ("level", po::value<int>(), "")
+    ;
+
+    po::store(po::parse_environment(env, "CFW_LOGGING_"), vm);
+    po::notify(vm);
+
+    if (vm.count("level")) {
+        std::cout << "Setting logging level to " 
+            << vm["level"].as<int>() << std::endl;
+    }
+}
 
 int main(int argc, char* argv[])
 {
+    // boost options parser, nice!
+    try {
+        parse_options(argc, argv);
+    } catch (std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return 1;
+    }
+
     std::vector<std::string> libs;
     libs.push_back("./libcfw_log.so");
 
     portal _portal;
 
     for (std::vector<std::string>::iterator it = libs.begin();
-       it != libs.end(); ++it) {
+            it != libs.end(); ++it) {
 
-       void* handle = dlopen((*it).c_str(), RTLD_LAZY);
-       if (!handle) {
-           std::cerr << "Cannot open library: " << dlerror() << std::endl;
-           return 1;
-       }
-       cfw_create_component_t create_component = (cfw_create_component_t) dlsym(handle, "cfw_create_component");
-       if (!create_component) {
-           std::cerr << "Cannot load symbol 'create_cfw_component': " << dlerror() << std::endl;
-           dlclose(handle);
-           return 1;
-       }
-       cfw::component* component = create_component(&_portal, "");
-       _portal.register_component(component);
+        void* handle = dlopen((*it).c_str(), RTLD_LAZY);
+        if (!handle) {
+            std::cerr << "Cannot open library: " << dlerror() << std::endl;
+            return 1;
+        }
+        cfw_create_component_t create_component = (cfw_create_component_t) dlsym(handle, "cfw_create_component");
+        if (!create_component) {
+            std::cerr << "Cannot load symbol 'create_cfw_component': " << dlerror() << std::endl;
+            dlclose(handle);
+            return 1;
+        }
+        cfw::component* component = create_component(&_portal, "");
+        _portal.register_component(component);
     } // !for
 
-	_portal.components().find(std::make_pair("log","1"))->second->start();	
-	_portal.components().find(std::make_pair("log","1"))->second->stop();	
+    _portal.components().find(std::make_pair("log","1"))->second->start();
+    _portal.components().find(std::make_pair("log","1"))->second->stop();
 
     return 0;
 } // main
